a260f63178be845787488a1e8ffc9c93b3044f5e
Renan Martins Canho <nextiwnl75@gmail.com>
2026-06-26 13:11:50 -0300

    test: realizando a construcao de uma nova feature de validar dataDeNascimento , usando o Test Driven Development

--- src/main/test/java/com/RegisterAccountTest.java
+++ src/main/test/java/com/RegisterAccountTest.java
@@ -10,14 +10,31 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RegisterAccountTest {
 
     @Test
-    void validarCpfDaConta(){
+    void validarCpfDaConta() {
         ContaBancaria conta = new ContaBancaria();
         String cpf = "54491805865";
 
-        if(cpf.length() <= 14){
+        if (cpf.length() <= 14) {
             conta.setCpf(cpf);
-        }System.out.println("cpf is invalid");
+        }
+        System.out.println("cpf is invalid");
 
-        assertEquals("54491805865",conta.getCpf());
+        assertEquals("54491805865", conta.getCpf());
     }
+
+    @Test
+    //void validarSeDataDeNascimentoMaiorQue18(){
+    ContaBancaria conta = new ContaBancaria();
+    int dataDeNascimento = 2007 - 03 - 31;
+    int idade = LocalDate.now().getYear() - dataDeNascimento;
+
+        if(idade>=18){
+            conta.setDataDeNascimento(dataDeNascimento);
+        }
+        System.out.println("data nascimento is invalid");
+
+    assertEquals(2007-03-31,conta.getDataDeNascimento());
 }
+
+
+
