6225f966626251bed6b5819a2abd8d2cc2b22b3b
Renan Martins Canho <nextiwnl75@gmail.com>
2026-06-28 20:05:36 -0300

    test: testando metodo que valida data de nascimento ,pra que seja permitido apenas usuarios maiores de 18 anos

--- src/test/java/RegisterAccountTest.java
+++ src/test/java/RegisterAccountTest.java
@@ -2,6 +2,7 @@ import contabancaria.ContaBancaria;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 
 
 class RegisterAccountTest {
@@ -18,8 +19,18 @@ class RegisterAccountTest {
 
             Assertions.assertEquals("54491805865", conta.getCpf());
         }
+    @Test
+        void validarDataNascimentoDaConta(){
+            ContaBancaria conta = new ContaBancaria();
+            LocalDate dataAtual = LocalDate.now();
+            LocalDate dataNascimento = LocalDate.EPOCH(1970, 3,31);
+            int idade = dataAtual.getYear() - dataAtual.getYear();
 
-
-
+                if(idade >= 18) {
+                    conta.setDataDeNascimento(dataNascimento);
+                }
+                System.out.println("data de nascimento is invalid");
+        }
     }
+
 
