267c1d4c1010c54e519ced138bd4593b0e18a451
Renan Martins Canho <nextiwnl75@gmail.com>
2026-06-30 12:32:31 -0300

    test: metodo validarDataNascimento aprovado no teste , pra ser implementando ao código , respeitando ao TDD

--- src/test/java/RegisterAccountTest.java
+++ src/test/java/RegisterAccountTest.java
@@ -20,17 +20,19 @@ class RegisterAccountTest {
             Assertions.assertEquals("54491805865", conta.getCpf());
         }
     @Test
-        void validarDataNascimentoDaConta(){
-            ContaBancaria conta = new ContaBancaria();
-            LocalDate dataAtual = LocalDate.now();
-            LocalDate dataNascimento = LocalDate.EPOCH(1970, 3,31);
-            int idade = dataAtual.getYear() - dataAtual.getYear();
+    void validarDataNascimentoDaConta() {
+        ContaBancaria conta = new ContaBancaria();
+        LocalDate dataNascimento = LocalDate.of(2007, 03, 31);
+        int idade = LocalDate.now().getYear() - dataNascimento.getYear();
 
-                if(idade >= 18) {
-                    conta.setDataDeNascimento(dataNascimento);
-                }
-                System.out.println("data de nascimento is invalid");
+        if (idade >= 18) {
+            conta.setDataDeNascimento(dataNascimento);
+        } else {
+            System.out.println("data de nascimento is invalid");
         }
+
+        Assertions.assertEquals(dataNascimento, conta.getDataDeNascimento());
     }
+}
 
 
